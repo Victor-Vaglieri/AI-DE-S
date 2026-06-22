7586026c232e5b35471a8bbf6660ef6ee904c1db
Victor-Vaglieri <victor_vglr@hotmail.com>
2026-06-22 11:36:08 -0300

    atualização do teste para somente esquema de vagas

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,5 +1,4 @@
 from app.schemas.jobs import JobListing, JobList
-from app.schemas.hardware import HardwarePrice, HardwareList
 
 def test_job_listing_defaults():
     job = JobListing()
@@ -9,17 +8,7 @@ def test_job_listing_defaults():
     assert job.salario == "Não informado"
     assert job.requisitos == []
 
-def test_hardware_price_defaults():
-    hw = HardwarePrice()
-    assert hw.produto == "Não informado"
-    assert hw.marca == "Desconhecida"
-    assert hw.preco_vista == 0.0
-    assert hw.em_estoque is False
-
 def test_job_list_empty():
     job_list = JobList()
     assert job_list.vagas == []
 
-def test_hardware_list_empty():
-    hw_list = HardwareList()
-    assert hw_list.produtos == []
