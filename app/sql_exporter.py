987ab5092eb934f16ed6db59909633943ffc23ba
Victor-Vaglieri <victor_vglr@hotmail.com>
2026-06-29 10:08:01 -0300

    correção dos imports e do tratamento de erro

--- app/sql_exporter.py
+++ app/sql_exporter.py
@@ -2,6 +2,7 @@ import logging
 import threading
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 from app.exporters_base import BaseExporter
 from app.models import Base, JobModel
 
@@ -17,31 +18,33 @@ class SqlExporter(BaseExporter):
         if self.db_url.startswith("sqlite"):
             connect_args = {"timeout": 30}
 
-        motor_banco = create_engine(self.db_url, connect_args=connect_args)
+        self.db_engine = create_engine(self.db_url, connect_args=connect_args)
         
         if self.db_url.startswith("sqlite"):
-            @event.listens_for(motor_banco, "connect")
+            @event.listens_for(self.db_engine, "connect")
             def set_sqlite_pragma(dbapi_connection, connection_record):
                 cursor = dbapi_connection.cursor()
                 cursor.execute("PRAGMA journal_mode=WAL")
                 cursor.execute("PRAGMA synchronous=NORMAL")
                 cursor.close()
 
-        Base.metadata.create_all(motor_banco)
-        self.Sessao = sessionmaker(bind=motor_banco)
+        Base.metadata.create_all(self.db_engine)
+        self.Session = sessionmaker(bind=self.db_engine)
 
     def save(self, data, mode):
         with self._lock:
-            sessao_atual = self.Sessao()
+            session = self.Session()
             try:
-                from sqlalchemy.exc import SQLAlchemyError
-                dados_vaga = data.model_dump()
-                dados_vaga['requisitos'] = ", ".join(dados_vaga.get('requisitos', []))
-                sessao_atual.add(JobModel(**dados_vaga))
-                sessao_atual.commit()
+                job_data = data.model_dump()
+                job_data['requisitos'] = ", ".join(job_data.get('requisitos', []))
+                session.add(JobModel(**job_data))
+                session.commit()
                 logger.debug("SQL: Vaga salva no banco")
-            except Exception as e:
-                sessao_atual.rollback()
+            except SQLAlchemyError as e:
+                session.rollback()
                 logger.error(f"Erro SQL: {e}")
+            except Exception as e:
+                session.rollback()
+                logger.error(f"Erro Inesperado: {e}")
             finally:
-                sessao_atual.close()
+                session.close()
